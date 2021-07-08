a48012a6abe90595ef8cb463d9b161a0bea85b8b
Yongveloper <aaa9015@naver.com>
2021-07-08 21:26:00 +0900

    1.0 React + Firebase Setup

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDBOHFb67vteS6nZr31oFplJGuQbWGdBhc",
+    authDomain: "ywitter-56849.firebaseapp.com",
+    projectId: "ywitter-56849",
+    storageBucket: "ywitter-56849.appspot.com",
+    messagingSenderId: "329686643872",
+    appId: "1:329686643872:web:c6b9dbb87a6b039b4f85c5"
+  };
+
+export default firebase.initializeApp(firebaseConfig);
