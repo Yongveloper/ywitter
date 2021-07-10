7acb6ecb2aacb1676ab2b0750329c7ffa5576c48
Yongveloper <aaa9015@naver.com>
2021-07-10 18:58:10 +0900

    3.2 Getting the Yweets

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,12 +1,28 @@
 import { dbService } from 'fbase';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Home = () => {
   const [yweet, setYweet] = useState('');
+  const [yweets, setYweets] = useState([]);
+
+  const getYweets = async () => {
+    const dbYweets = await dbService.collection('yweets').get();
+    dbYweets.forEach((document) => {
+      const yeetsObject = {
+        ...document.data(),
+        id: document.id,
+      };
+      setYweets((prev) => [yeetsObject, ...prev]);
+    });
+  };
+
+  useEffect(() => {
+    getYweets();
+  }, []);
 
   const onSubmit = async (event) => {
     event.preventDefault();
-    await dbService.collection('nweets').add({
+    await dbService.collection('yweets').add({
       yweet,
       createAt: Date.now(),
     });
@@ -28,10 +44,17 @@ const Home = () => {
           onChange={onChange}
           type="text"
           placeholder="What's on your mind?"
-          maxlength={120}
+          maxLength={120}
         />
         <input type="submit" value="Yweet" />
       </form>
+      <div>
+        {yweets.map((yweet) => (
+          <div key={yweet.id}>
+            <h4>{yweet.yweet}</h4>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
