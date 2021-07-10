adcbfdd5172f655cf0884b2cbad855d8823a632c
Yongveloper <aaa9015@naver.com>
2021-07-10 18:37:30 +0900

    3.1 Yweeting

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,7 +1,39 @@
-import React from 'react';
+import { dbService } from 'fbase';
+import React, { useState } from 'react';
 
 const Home = () => {
-  return <div>Home</div>;
+  const [yweet, setYweet] = useState('');
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    await dbService.collection('nweets').add({
+      yweet,
+      createAt: Date.now(),
+    });
+    setYweet('');
+  };
+
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setYweet(value);
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          value={yweet}
+          onChange={onChange}
+          type="text"
+          placeholder="What's on your mind?"
+          maxlength={120}
+        />
+        <input type="submit" value="Yweet" />
+      </form>
+    </div>
+  );
 };
 
 export default Home;
